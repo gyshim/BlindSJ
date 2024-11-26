d413510a6f8ad8eba6edd639767ab301578a733d
gyshim <gyshim1004@gmail.com>
2024-11-26 20:12:11 +0900

    change user ID

--- app/src/main/java/com/growstudio/android/blindsj/MainActivity.kt
+++ app/src/main/java/com/growstudio/android/blindsj/MainActivity.kt
@@ -13,12 +13,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.growstudio.android.blindsj.ui.theme.BlindSJTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainActivity : ComponentActivity() { // componentActivity는 컴포즈에서 ui를 작성하는 기본 액티비티
+    override fun onCreate(savedInstanceState: Bundle?) { // componentActivity의 onCreate 메서드를 재정의
+        super.onCreate(savedInstanceState) // 부모의 기본 동작 (원본 onCreate)
         enableEdgeToEdge()
         setContent {
-            BlindSJTheme {
+            BlindSJTheme {  // 앱의 테마
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
